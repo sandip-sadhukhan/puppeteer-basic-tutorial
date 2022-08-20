dd55089d9ae67c70a76df0c2108053d251919159
Sandip Sadhukhan <sandippakhanna@gmail.com>
2022-08-20 19:35:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Basic Tutoral on Pupetters
+
+### How to run?
+
+```bash
+# install all dependencies
+yarn install
+
+# run
+yarn start
+```
---
+++ tests/example.test.js
@@ -0,0 +1,33 @@
+const puppeteer = require("puppeteer")
+
+describe("My First Puppeteer Test", () => {
+	it("should launch the browser", async function () {
+        // Initilize the browser
+		const browser = await puppeteer.launch({
+			headless: false, // will open browser physically
+			slowMo: 10, // will add 10ms before excuting program
+			devtools: false, // devtools: true will open devtools by default
+		})
+
+        // create a new page on browser
+		const page = await browser.newPage()
+
+        // goto this url
+		await page.goto("http://example.com/")
+
+        // wait for 3s
+        await page.waitForTimeout(3000)
+
+        // wait for an element
+		await page.waitForSelector("h1")
+
+        // reload the page
+        await page.reload()
+
+        await page.waitForTimeout(3000)
+        await page.waitForSelector('h1')
+
+        // close the browser
+		await browser.close()
+	})
+})
